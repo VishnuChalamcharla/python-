60124924497a9b512be2afe730dc3c3328e992ea
vishnu chalamcharla <vishnuchalamcharla7@gmail.com>
2024-11-21 19:41:53 +0530

    Your commit message

---
+++ sort.py
@@ -0,0 +1,9 @@
+# Sorting a list of numbers
+numbers = [5, 2, 9, 1, 5, 6]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+# Sorting a list of strings alphabetically
+words = ["banana", "apple", "cherry"]
+sorted_words = sorted(words)
+print(sorted_words)
